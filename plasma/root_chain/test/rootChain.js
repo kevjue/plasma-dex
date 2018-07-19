f8c4c8c7e0a8cdd3ee60ee5e13b2b73ccf4a95ae
Kevin Jue <kjue235@gmail.com>
2018-07-19 12:12:47 +0000

    added test cases for exits of eth and token deposits

--- plasma/root_chain/test/rootChain.js
+++ plasma/root_chain/test/rootChain.js
@@ -32,10 +32,20 @@ contract('RootChain', async (accounts) => {
 	return web3.sha3(args, { encoding: 'hex' })
     };
 
+    function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+    const OPERATOR_ADDRESS = accounts[0];
+    const USER_ADDRESS = accounts[1];
+    var ethDepositBlockNum;
+    var tokenDepositBlockNum;
+    
 
     it("test for eth deposit into root chain", async () => {
-	let txnInfo = await rootChain.depositEth({from: accounts[0], value: web3.toWei(1, 'ether')});
+	ethDepositBlockNum = await rootChain.getDepositBlock().then(function(num) {return num.toNumber()});
+	let txnInfo = await rootChain.depositEth({from: USER_ADDRESS, value: web3.toWei(1, 'ether')});
 	let rootChainBalance = web3.eth.getBalance(rootChain.address);
 
 	// Verify that the root chain smart contract balance is 1 Eth
@@ -51,16 +61,164 @@ contract('RootChain', async (accounts) => {
 	let token = txnInfo.logs[0].args.token;
 	let amount = txnInfo.logs[0].args.amount;
 	assert.isTrue((eventType == 'Deposit') &&
-		      (depositor == accounts[0]) &&
-		      (depositBlockNum == 1) && 
+		      (depositor == USER_ADDRESS) &&
+		      (depositBlockNum == ethDepositBlockNum) && 
 		      (token == ZERO_ADDRESS) &&
 		      (amount == web3.toWei(1, 'ether')), 'emitted event incorrect')
 
 	// Verify that the contents of the deposit block is correct
 	let depositBlock = await rootChain.getChildChain(depositBlockNum);
-	assert.equal(depositBlock[0], keccak256(accounts[0], ZERO_ADDRESS, parseInt(web3.toWei(1, 'ether'))),
+	assert.equal(depositBlock[0], keccak256(USER_ADDRESS, ZERO_ADDRESS, parseInt(web3.toWei(1, 'ether'))),
 		     'deposit block contents incorrect');
+    });
 
-	
+    it("test for token deposit into root chain", async () => {
+	// TODO:  Test for unapproved deposit
+	await pdexToken.transfer(USER_ADDRESS, web3.toWei(1, 'ether'),    // Hack. This works since the number of decimals for
+		  	                                                  // pdexToken is the same as ether.
+				 {'from': OPERATOR_ADDRESS});
+	await pdexToken.approve(rootChain.address,
+				web3.toWei(1, 'ether'),
+				{from: USER_ADDRESS});
+	tokenDepositBlockNum = await rootChain.getDepositBlock().then(function(num) {return num.toNumber()});
+	let txnInfo = await rootChain.depositToken(web3.toWei(1, 'ether'),
+						   {from: USER_ADDRESS});
+
+	let rootChainBalance = await pdexToken.balanceOf(rootChain.address);
+
+	// Verify that the root chain smart contract balance is 1 Eth
+	assert.equal(rootChainBalance, web3.toWei(1, 'ether'), "token balance in root chain contract incorrect");
+
+	// Verify that there is one event from the transaction
+	assert.equal(txnInfo.logs.length, 1, "exactly 1 event should have been emitted");
+
+	// Verify the emitted event is correct
+	let eventType = txnInfo.logs[0].event;
+	let depositor = txnInfo.logs[0].args.depositor;
+	let depositBlockNum = txnInfo.logs[0].args.depositBlock;
+	let token = txnInfo.logs[0].args.token;
+	let amount = txnInfo.logs[0].args.amount;
+	assert.isTrue((eventType == 'Deposit') &&
+		      (depositor == USER_ADDRESS) &&
+		      (depositBlockNum == tokenDepositBlockNum) && 
+		      (token == pdexToken.address) &&
+		      (amount == web3.toWei(1, 'ether')), 'emitted event incorrect')
+
+	// Verify that the contents of the deposit block is correct
+	let depositBlock = await rootChain.getChildChain(depositBlockNum);
+	assert.equal(depositBlock[0], keccak256(USER_ADDRESS, pdexToken.address, parseInt(web3.toWei(1, 'ether'))),
+		     'token deposit block contents incorrect');
+    });
+    
+    it("test for eth deposit exit from root chain", async () => {
+	// TODO:  Test for incorrect exit amount
+	let ethDepositPos = ethDepositBlockNum * 1000000000;
+	let txnInfo = await rootChain.startDepositExit(ethDepositPos,
+						       ZERO_ADDRESS,
+						       web3.toWei(1, 'ether'),
+						       {'from': USER_ADDRESS});
+
+	// Verify that there is one event from the transaction
+	assert.equal(txnInfo.logs.length, 1, "exactly 1 event should have been emitted");
+
+	// Verify the emitted event is correct
+	let eventType = txnInfo.logs[0].event;
+	let exitor = txnInfo.logs[0].args.exitor;
+	let utxoPos = txnInfo.logs[0].args.utxoPos;
+	let token = txnInfo.logs[0].args.token;
+	let amount = txnInfo.logs[0].args.amount;
+	assert.isTrue((eventType == 'ExitStarted') &&
+		      (exitor == USER_ADDRESS) &&
+		      (utxoPos == ethDepositPos) && 
+		      (token == ZERO_ADDRESS) &&
+		      (amount == web3.toWei(1, 'ether')), 'emitted event incorrect')
+
+	let exitInfo = await rootChain.getExit(ethDepositPos);
+
+	// Verify that the exitInfo is correct
+	assert.isTrue((exitInfo[0] == USER_ADDRESS) &&
+		      (exitInfo[1] == ZERO_ADDRESS) &&
+		      (exitInfo[2] == web3.toWei(1, 'ether')), "exit info is incorrect")
+
+	// TODO:  Test for too early exit
+	let minExitTime = await rootChain.minExitTime().then(function(time) {return time.toNumber()});
+	await sleep(2 * minExitTime * 1000);
+
+	let userBalance = web3.eth.getBalance(USER_ADDRESS);
+
+	txnInfo = await rootChain.finalizeExits(ZERO_ADDRESS, {'from': OPERATOR_ADDRESS});
+	// Verify that there is one event from the transaction
+	assert.equal(txnInfo.logs.length, 1, "exactly 1 event should have been emitted for finalizeExits");
+
+	// Verify the emitted event is correct
+	eventType = txnInfo.logs[0].event;
+	exitor = txnInfo.logs[0].args.exitor;
+	utxoPos = txnInfo.logs[0].args.utxoPos;
+	token = txnInfo.logs[0].args.token;
+	amount = txnInfo.logs[0].args.amount;
+	assert.isTrue((eventType == 'ExitFinalized') &&
+		      (exitor == USER_ADDRESS) &&
+		      (utxoPos == ethDepositPos) &&
+		      (token == ZERO_ADDRESS) &&
+		      (amount == web3.toWei(1, 'ether')), 'emitted event incorrect');
+
+	let newUserBalance = web3.eth.getBalance(USER_ADDRESS);
+	assert.equal(newUserBalance - userBalance, web3.toWei(1, 'ether'), "Exit amount incorrect");
+    });
+
+    it("test for token deposit exit from root chain", async () => {
+	// TODO:  Test for incorrect exit amount
+	let tokenDepositPos = tokenDepositBlockNum * 1000000000;
+	let txnInfo = await rootChain.startDepositExit(tokenDepositPos,
+						       pdexToken.address,
+						       web3.toWei(1, 'ether'),
+						       {'from': USER_ADDRESS});
+
+	// Verify that there is one event from the transaction
+	assert.equal(txnInfo.logs.length, 1, "exactly 1 event should have been emitted");
+
+	// Verify the emitted event is correct
+	let eventType = txnInfo.logs[0].event;
+	let exitor = txnInfo.logs[0].args.exitor;
+	let utxoPos = txnInfo.logs[0].args.utxoPos;
+	let token = txnInfo.logs[0].args.token;
+	let amount = txnInfo.logs[0].args.amount;
+	assert.isTrue((eventType == 'ExitStarted') &&
+		      (exitor == USER_ADDRESS) &&
+		      (utxoPos == tokenDepositPos) && 
+		      (token == pdexToken.address) &&
+		      (amount == web3.toWei(1, 'ether')), 'emitted event incorrect')
+
+	let exitInfo = await rootChain.getExit(tokenDepositPos);
+
+	// Verify that the exitInfo is correct
+	assert.isTrue((exitInfo[0] == USER_ADDRESS) &&
+		      (exitInfo[1] == pdexToken.address) &&
+		      (exitInfo[2] == web3.toWei(1, 'ether')), "exit info is incorrect")
+
+	// TODO:  Test for too early exit
+	let minExitTime = await rootChain.minExitTime().then(function(time) {return time.toNumber()});
+	await sleep(2 * minExitTime * 1000);
+
+	let userBalance = await pdexToken.balanceOf(USER_ADDRESS);
+
+	txnInfo = await rootChain.finalizeExits(pdexToken.address, {'from': OPERATOR_ADDRESS});
+	// Verify that there is one event from the transaction
+	assert.equal(txnInfo.logs.length, 1, "exactly 1 event should have been emitted for finalizeExits");
+
+	// Verify the emitted event is correct
+	eventType = txnInfo.logs[0].event;
+	exitor = txnInfo.logs[0].args.exitor;
+	utxoPos = txnInfo.logs[0].args.utxoPos;
+	token = txnInfo.logs[0].args.token;
+	amount = txnInfo.logs[0].args.amount;
+	assert.isTrue((eventType == 'ExitFinalized') &&
+		      (exitor == USER_ADDRESS) &&
+		      (utxoPos == tokenDepositPos) &&
+		      (token == pdexToken.address) &&
+		      (amount == web3.toWei(1, 'ether')), 'emitted event incorrect');
+
+	let newUserBalance = await pdexToken.balanceOf(USER_ADDRESS);
+	assert.equal(newUserBalance - userBalance, web3.toWei(1, 'ether'), "Exit amount incorrect");
     });
 });
