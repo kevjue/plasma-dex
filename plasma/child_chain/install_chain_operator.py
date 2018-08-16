e2d9c7340e12bcf5f1e9790cc6b3e7d383b7e0e6
Kevin Jue <kjue235@gmail.com>
2018-08-16 07:29:46 -0700

    final bug fixes for chilc chain deployment scripts

--- plasma/child_chain/install_chain_operator.py
+++ plasma/child_chain/install_chain_operator.py
@@ -1,13 +1,14 @@
 import click
 import os
+import site
 
 @click.command()
 @click.option('--root_chain_address', help="The ethereum address of the root chain smart contract", required=True)
 def main(root_chain_address):
-    crontab_cmd = "printf '* * * * * ( cd %%s/plasma-dex/plasma/child_chain; env ROOT_CHAIN_ADDRESS=%s PYTHONPATH=%%s/plasma-dex pipenv submit_block >> %%s/submitblockcron.output  2>>%%s/submitblockcron.err\\n' $HOME $HOME $HOME $HOME | crontab" % root_chain_address
+    crontab_cmd = "printf '* * * * * ( cd %%s/plasma-dex/plasma/; env ROOT_CHAIN_ADDRESS=%s PYTHONPATH=%%s/plasma-dex %s/bin/pipenv run submit_block >> %%s/submitblockcron.output  2>>%%s/submitblockcron.err )\\n' $HOME $HOME $HOME $HOME | crontab" % (root_chain_address, site.USER_BASE)
     print(crontab_cmd)
-    #os.system(crontab_cmd)
+    os.system(crontab_cmd)
 
-
+    
 if __name__ == '__main__':
     main()
