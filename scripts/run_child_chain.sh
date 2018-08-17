d53a90d589fcb68de8b02a14dcccd7f28aed8f13
Kevin Jue <kjue235@gmail.com>
2018-08-16 23:02:35 -0700

    fixed run_child_chain.sh

--- scripts/run_child_chain.sh
+++ scripts/run_child_chain.sh
@@ -3,6 +3,6 @@
 cd ~/plasma-dex/plasma/;
 pip3 install --user pipenv;
 pipenv install;
-python3 ~/plasma-dex/plasma/child_chain/install_chain_operator.py --root_chain_address=$1;
-env PYTHONPATH=~/plasma-dex pipenv run python3 child_chain/server.py --root_chain_address=$1 --eth_node_endpoint=http://localhost:8545;
+env PYTHONPATH=$HOME/plasma-dex pipenv run python3 child_chain/install_chain_operator.py --root_chain_address=$1;
+env PYTHONPATH=$HOME/plasma-dex pipenv run python3 child_chain/server.py --root_chain_address=$1 --eth_node_endpoint=http://localhost:8545;
 
