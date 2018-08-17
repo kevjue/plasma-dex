b7166ad764099c303766d8fee9861ad13157922f
Kevin Jue <kjue235@gmail.com>
2018-08-17 01:47:06 -0700

    modified smart contract migration scripts to have accounts[0] deposit 100 tokens to root chain

--- plasma/root_chain/migrations/3_deploy_root_chain.js
+++ plasma/root_chain/migrations/3_deploy_root_chain.js
@@ -4,13 +4,25 @@ var pdex_token = artifacts.require("PDEXToken");
 var root_chain = artifacts.require("RootChain");
 
 module.exports = function(deployer, network, accounts) {
+    var deployed_pdex_token;
+    var deployed_root_chain;
 
     deployer.then(function() {
 
 	// load the deployed token
 	return pdex_token.deployed();
-    }).then(function(pdex_token) {
+    }).then(function(_deployed_pdex_token) {
         // deploy the root chain from account zero
-	return deployer.deploy(root_chain, pdex_token.address, {from: accounts[0], gasPrice: 10000000000});
+	deployed_pdex_token = _deployed_pdex_token;
+	return deployer.deploy(root_chain, deployed_pdex_token.address, {from: accounts[0], gasPrice: 10000000000});
+    }).then(function() {
+	return root_chain.deployed();
+    }).then(function(_deployed_root_chain) {
+	deployed_root_chain = _deployed_root_chain;
+	deployed_pdex_token.approve(deployed_root_chain.address, web3.toWei(100, 'ether'), {from: accounts[0]});
+    }).then(function() {
+	deployed_root_chain.depositToken(web3.toWei(100, 'ether'), {from:accounts[0]});
+    }).then(function() {
+	console.log(accounts[0] + " deposited 100 PDEX tokens to root chain");
     });
 };	   
